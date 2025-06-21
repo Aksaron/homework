03d04be2f98b9fce874e56163a34a9db6f921d00
Aksaron <linkorqzw@gmail.com>
2025-06-21 12:30:59 +0300

    Add files via upload

--- Wake & Bake/js/main.js
+++ Wake & Bake/js/main.js
@@ -78,34 +78,64 @@
 
   const accordionLists = document.querySelectorAll('.accordion-list');
 
-    accordionLists.forEach(el => {
+  accordionLists.forEach(el => {
 
-        el.addEventListener('click', (e) => {
+    el.addEventListener('click', (e) => {
 
-            const accordionList = e.currentTarget
-            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
-            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+      const accordionList = e.currentTarget
+      const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+      const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
 
-            const accordionControl = e.target.closest('.accordion-list__control');
-            if (!accordionControl) return
-            const accordionItem = accordionControl.parentElement;
-            const accordionContent = accordionControl.nextElementSibling;
+      const accordionControl = e.target.closest('.accordion-list__control');
+      if (!accordionControl) return
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
 
-            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
-                accordionOpenedItem.classList.remove('accordion-list__item--opened');
-                accordionOpenedContent.style.maxHeight = null;
-            }
-            accordionItem.classList.toggle('accordion-list__item--opened');
+      if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+        accordionOpenedItem.classList.remove('accordion-list__item--opened');
+        accordionOpenedContent.style.maxHeight = null;
+      }
+      accordionItem.classList.toggle('accordion-list__item--opened');
 
-            if (accordionItem.classList.contains('accordion-list__item--opened')) {
-                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
-            } else {
-                accordionContent.style.maxHeight = null;
-            }
-
-        });
+      if (accordionItem.classList.contains('accordion-list__item--opened')) {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else {
+        accordionContent.style.maxHeight = null;
+      }
 
     });
+
+  });
+
+  /* ====================Слайдер&Галерея============================== */
+
+  const swiper = new Swiper('.gallery__slider', {
+
+    spaceBetween: 15,
+    slidesPerView: 1.5,
+
+    pagination: {
+      el: '.gallery__pagination',
+      type: 'fraction',
+    },
+
+    navigation: {
+      nextEl: '.gallery__next',
+      prevEl: '.gallery__prev',
+    },
+
+      breakpoints: {
+        601: {
+          slidesPerView: 3,
+        },
+        801: {
+          spaceBetween: 32,
+        },
+        1101: {
+          slidesPerView: 4,
+        }
+      }
+  });
 
 })()
 
