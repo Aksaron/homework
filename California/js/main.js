1844fd04330446cbecea55371d89336b01f733f2
Aksaron <linkorqzw@gmail.com>
2025-07-14 13:27:24 +0300

    Add files via upload

--- California/js/main.js
+++ California/js/main.js
@@ -1,15 +1,82 @@
-const swiper = new Swiper('.hero__swiper', {
-  pagination: {
-    el: '.hero__pagination',
-    type: 'bullets',
-  },
-  navigation: {
-    nextEl: '.hero__next',
-    prevEl: '.hero__prev',
-  },
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  speed: 1500,
+(function () {
+
+
+  
+  const swiper = new Swiper('.hero__swiper', {
+    
+    loop: true,
+
+    pagination: {
+      el: '.hero__pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.hero__next',
+      prevEl: '.hero__prev',
+    },
+    /* autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    speed: 1500, */
+  });
+  
+  // ////////////////////////////// Drop Nav Menu
+const dropdownButton = document.getElementById('dropdown__btn');
+const dropdownMenu = document.getElementById('dropdown-menu');
+
+dropdownButton.addEventListener('click', () => {
+    dropdownMenu.classList.toggle('active');
 });
+/////////////////////// Burger drop menu
+const dropBurgButton = document.getElementById('dropBurg__btn');
+const dropBurgMenu = document.getElementById('dropBurg-menu');
+
+// Добавляем обработчик события на кнопку
+dropBurgButton.addEventListener('click', () => {
+    dropBurgMenu.classList.toggle('opened');
+    
+});
+
+  /* =================Бургер===================== */
+  document.addEventListener('click', burgerInit)
+  
+  function burgerInit(e) {
+    
+    const burgerIcon = e.target.closest('.burger-icon')
+    const burgerNavLink = e.target.closest('.nav__link')
+    
+    if (!burgerIcon && !burgerNavLink) return
+    if (document.documentElement.clientWidth > 900) return
+    
+    if (!document.body.classList.contains('body--opened-menu')) {
+      document.body.classList.add('body--opened-menu')
+    } else {
+      document.body.classList.remove('body--opened-menu')
+    }
+    
+  }
+  /* =====================Модалка========================== */
+  const modal = document.querySelector('.modal')
+  const modalButton = document.querySelector('.about__img-button')
+
+
+  modalButton.addEventListener('click', openModal)
+  modal.addEventListener('click', closeModal)
+
+  function openModal(e) {
+    e.preventDefault()
+    document.body.classList.toggle('body--opened-modal')
+  }
+
+  function closeModal(e) {
+    e.preventDefault()
+
+    const target = e.target
+
+    if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+      document.body.classList.remove('body--opened-modal')
+    }
+
+  }
+})()
