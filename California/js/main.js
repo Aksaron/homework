cd30a0344fe4f9f64e3998d86db0407d0846faed
Aksaron <linkorqzw@gmail.com>
2025-07-15 16:36:02 +0300

    Add files via upload

--- California/js/main.js
+++ California/js/main.js
@@ -48,26 +48,25 @@ if (burgerNavLink && burgerNavLink.hasAttribute('data-dropdown-link')) {
 }
 
   /* =====================Модалка========================== */
-  const modal = document.querySelector('.modal')
-  const modalButton = document.querySelector('.about__img-button')
+  const modal = document.querySelector('.modal');
+const modalButton = document.querySelector('.about__img-button');
 
+if (modal && modalButton) {
+  modalButton.addEventListener('click', openModal);
+  modal.addEventListener('click', closeModal);
+}
 
-  modalButton.addEventListener('click', openModal)
-  modal.addEventListener('click', closeModal)
+function openModal(e) {
+  e.preventDefault();
+  document.body.classList.toggle('body--opened-modal');
+}
 
-  function openModal(e) {
-    e.preventDefault()
-    document.body.classList.toggle('body--opened-modal')
+function closeModal(e) {
+  e.preventDefault();
+  const target = e.target;
+  if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+    document.body.classList.remove('body--opened-modal');
   }
+}
 
-  function closeModal(e) {
-    e.preventDefault()
-
-    const target = e.target
-
-    if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
-      document.body.classList.remove('body--opened-modal')
-    }
-
-  }
 })()
